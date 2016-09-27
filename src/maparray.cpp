29e7b08f78cc883a649ae757645c2ce80a62f3a0
Mark Jenkins <Jink19v@gmail.com>
2016-09-27 23:15:22 +0100

    Still attempting passing args

--- src/maparray.cpp
+++ src/maparray.cpp
@@ -5,23 +5,10 @@
 #include <pthread.h>
 using namespace std;
 
-#define DEBUG 
-#define NUM_THREADS 1
+#define DEBUG
+#define NUM_THREADS 5
 
-template <typename T1, typename T2, typename T3>
-struct threadDataStruct {
-   int threadId;
-
-   int startIndex;
-   int numTasks;
-
-   vector<T1>& input1;
-   vector<T2>& input2;
-   vector<T3>& output;
-   T3 (*function)(T1, vector<T2>&);
-};
-
-// Test function for map array. Returns sum of the product of val with each number in vect
+// Test function for map array. Returns sum of the product of val with each number in vect, produces a perfectly balanced workload
 double testFunction(int val, vector<int>& vect)
 {
    int output = 0;
@@ -33,72 +20,97 @@ double testFunction(int val, vector<int>& vect)
 }
 
 template <typename T1, typename T2, typename T3>
-// void mapArrayThreadTemplate(T3 (function)(T1, vector<T2>&), vector<T1>& input1, vector<T2>& input2, vector<T3>& output, int startIndex, int numTasks)
+struct threadDataStruct {
+   int threadId;
+
+   int startIndex;
+   int numTasks;
+
+   vector<T1> *input1;
+   vector<T2> *input2;
+   T3 (*function)(T1, vector<T2>&);
+   vector<T3> *output;
+};
+
+template <typename T1, typename T2, typename T3>
 void *mapArrayThread(void *threadData)
 {
-   // struct threadDataStruct<T1, T2, T3> *data = (threadDataStruct<T1, T2, T3>) threadData;
-   
-   int id = threadData->threadId;
+   struct threadDataStruct<T1, T2, T3> *data = (struct threadDataStruct<T1, T2, T3> *) threadData;
 
-   // cout << "Thread data:\n" << data->threadId << endl << data->startIndex << endl << data->numTasks << endl << data->input1[3] << endl << data->input2[3] << endl << data->output[3] << endl;
+   cout << "[User Thread " << data->threadId << "]: " << endl << "startIndex: " << data->startIndex << endl << "numTasks: " << data->numTasks << endl;
 
-   // for (int i = 0; i < data->numTasks; i++) {
-   //    data->output[data->startIndex + i] = function(data->input1[i], data->input2);
-   // }
+   for (int i = 0; i < data->numTasks; i++) {
+      // data->output[data->startIndex + i] = testFunction(data->input1[i], data->input2);
+   }
+
+   pthread_exit(NULL);
 }
 
 template <typename T1, typename T2, typename T3>
-void mapArray(T3 (function)(T1, vector<T2>&), vector<T1>& input1, vector<T2>& input2, vector<T3>& output)
+void mapArray(vector<T1>& input1, vector<T2>& input2, T3 (function)(T1, vector<T2>&), vector<T3>& output)
 {
-   assert(input1.size() == output.size() && "input1 and output array sizes must be equal!");
+   assert(input1.size() == output.size() && "Input1 and output array sizes must be equal!");
 
    int base  = input1.size() / NUM_THREADS;
    int extra = input1.size() % NUM_THREADS;
+   int baseTotal = 0;
 
    pthread_t threads[NUM_THREADS];
-   // struct threadDataStruct<T1, T2, T3> *threadData[NUM_THREADS];
-   struct threadDataStruct<T1, T2, T3> *threadData;
+   struct threadDataStruct<T1, T2, T3> threadData[NUM_THREADS];
 
-   threadData->threadId = 1;
+   for (int i = 0; i < NUM_THREADS; i++ ) {
+      // Writing data to pass to thread
+      threadData[i].threadId   = i;
+      threadData[i].startIndex = baseTotal;
 
-   int rc;
+      if (extra > 0) {
+         threadData[i].numTasks = base + 1;
+         extra--;
+      } else {
+         threadData[i].numTasks = base;
+      }
 
-   for(int i = 0; i < NUM_THREADS; i++) {
-      cout <<"main() : creating thread, " << i << endl;
+      baseTotal += threadData[i].numTasks;
 
-      // threadData[i]->threadId = i;
-      // threadData.push_back(threadDataClass<T1, T2, T3>(i, 0, input1.size(), input1, input2, output, function));
+      threadData[i].input1     = &input1;
+      cout << "HEY " << threadData[i]->input1 << endl;
+      threadData[i].input2     = &input2;
+      threadData[i].function   = function;
+      threadData[i].output     = &output;
 
-      rc = pthread_create(&threads[i], NULL, mapArrayThread<T1, T2, T3>, (void*) &threadData);
-      if (rc){
-         cout << "Error:unable to create thread," << rc << endl;
+      cout << "[User Program_]: Creating thread " << i << endl;
+
+      // Create our thread, and pass a pointer to it's threadDataStruct
+      int err = pthread_create(&threads[i], NULL, mapArrayThread<T1, T2, T3>, (void *)&threadData[i]);
+
+      // Checking for errors creating the thread
+      if (err) {
+         cout << "Error:unable to create thread, " << err << endl;
          exit(-1);
       }
    }
-
-   // for (int i = 0; i < input1.size(); i++) {
-   //    output[i] = function(input1[i], input2);
-   // }
+   pthread_exit(NULL);
 }
- 
+
 int main()
 {
    // Test input vectors
    vector<int> input1;
    vector<int> input2;
 
-   int i;
-
-   // Push 5 values into the vectors
-   for(i = 0; i < 10; i++){
+   // Push values onto the vectors
+   for (int i = 0; i < 13; i++) {
       input1.push_back(i);
       input2.push_back(i);
    }
 
+   // Create output vector
    vector<double> output(input1.size());
 
-   mapArray(testFunction, input1, input2, output);
+   // Run mapArray
+   mapArray(input1, input2, testFunction, output);
 
+   // Print input/output vectors
    for (vector<int>::const_iterator i = input1.begin(); i != input1.end(); ++i)
       cout << *i << ' ';
 
