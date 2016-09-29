929f5b5f499a3ab60f41d192bf282c42cd8a6819
Mark Jenkins <Jink19v@gmail.com>
2016-09-29 19:07:44 +0100

    Progress on passing function to threads

--- src/maparray.cpp
+++ src/maparray.cpp
@@ -8,89 +8,159 @@ using namespace std;
 #define DEBUG
 #define NUM_THREADS 5
 
-// Test function for map array. Returns sum of the product of val with each number in vect, produces a perfectly balanced workload
-double testFunction(int val, vector<int>& vect)
-{
-   int output = 0;
 
-   for (vector<int>::const_iterator i = vect.begin(); i != vect.end(); ++i)
-      output += val * *i;
 
-   return output;
-}
+/*
+ *  Macro definition for user functions. The definition expands as a struct which can be used when 
+ *  creating new patterns. Note - \ lets you continue a statement onto the next line, so we can 
+ *  write #define statements on multiple lines like this.
+ *
+ */
+#define USER_FUNCTION(name, inType1, in1, inType2, in2, outType, function)\
+struct name\
+{\
+    typedef inType1 IT1;\
+    typedef inType2 IT2;\
+    typedef outType OT;\
+    \
+    inline outType execute(inType1 in1, vector<inType2>& in2)\
+    {\
+        function\
+    }\
+};
 
-template <typename T1, typename T2, typename T3>
+
+
+/* 
+ *  A quick test function for map array. Returns sum of the product of val with each number in vect,
+ *  produces a perfectly balanced workload.
+ *
+ */
+USER_FUNCTION(testFunction, int, i1, int, i2, int, 
+
+              int output = 0;
+
+              for (vector<int>::const_iterator i = i2.begin(); i != i2.end(); ++i)
+                output += i1 * *i;
+
+              return output;)
+
+// template <typename T1, typename T2, typename T3>
 struct threadDataStruct {
    int threadId;
 
    int startIndex;
    int numTasks;
+   // it1 test;
 
-   vector<T1> *input1;
-   vector<T2> *input2;
-   T3 (*function)(T1, vector<T2>&);
-   vector<T3> *output;
+   // vector<it1> *input1;
+   // vector<IT2> *input2;
+   // vector<OT> *output;
 };
 
-template <typename T1, typename T2, typename T3>
-void *mapArrayThread(void *threadData)
+/*!
+ *  \class MapArray
+ *
+ *  \brief A class representing the MapArray skeleton.
+ *
+ *  This class defines the MapArray skeleton. MapArray is yet another variant of Map.
+ *  It produces a result (vector/matrix) from either two input objects (vector/matrix)
+ *  where each element of the result, is a
+ *   function of the corresponding element of the second input (vector/matrix),
+ *  and any number of elements from the first input (vector). This means that at each call to the user defined function, which is
+ *  done for each element in input two, all elements from input one can be accessed.
+ *  Once instantiated, it is meant to be used as a function and therefore overloading
+ *  \p operator(). There are a few overloaded variants of this operator. One using containers as inputs and the other using
+ *  iterators to define a range of elements.
+ *
+ *  If a certain backend is to be used, functions with the same interface as \p operator() but by the names \p CPU, \p CU,
+ *  \p CL, \p OMP exists for CPU, CUDA, OpenCL and OpenMP respectively.
+ *
+ *  The MapArray skeleton also includes a pointer to an Environment which includes the devices available to execute on.
+ */
+template <typename MapArrayFunc>
+class MapArray
 {
-   struct threadDataStruct<T1, T2, T3> *data = (struct threadDataStruct<T1, T2, T3> *) threadData;
 
-   cout << "[User Thread " << data->threadId << "]: " << endl << "startIndex: " << data->startIndex << endl << "numTasks: " << data->numTasks << endl;
+public:
+   MapArray(MapArrayFunc* mapArrayFunc);
 
-   for (int i = 0; i < data->numTasks; i++) {
-      // data->output[data->startIndex + i] = testFunction(data->input1[i], data->input2);
-   }
+   ~MapArray();
 
-   pthread_exit(NULL);
-}
+private:
+   MapArrayFunc* m_mapArrayFunc;
 
-template <typename T1, typename T2, typename T3>
-void mapArray(vector<T1>& input1, vector<T2>& input2, T3 (function)(T1, vector<T2>&), vector<T3>& output)
-{
-   assert(input1.size() == output.size() && "Input1 and output array sizes must be equal!");
+public:
+   template <typename T>
+   void OMP(vector<T>& input1, vector<T>& input2, vector<T>& output);
 
-   int base  = input1.size() / NUM_THREADS;
-   int extra = input1.size() % NUM_THREADS;
-   int baseTotal = 0;
+   template <typename Input1Iterator, typename Input2Iterator, typename OutputIterator>
+   void OMP(Input1Iterator input1Begin, Input1Iterator input1End, Input2Iterator input2Begin, Input2Iterator input2End, OutputIterator outputBegin);
+};
 
-   pthread_t threads[NUM_THREADS];
-   struct threadDataStruct<T1, T2, T3> threadData[NUM_THREADS];
+// template <typename T1, typename T2, typename T3>
+// void *mapArrayThread(void *threadData)
+// {
+//    // struct threadDataStruct<T1, T2, T3> *data = (struct threadDataStruct<T1, T2, T3> *) threadData;
+//    struct threadDataStruct *data = (struct threadDataStruct *) threadData;
 
-   for (int i = 0; i < NUM_THREADS; i++ ) {
-      // Writing data to pass to thread
-      threadData[i].threadId   = i;
-      threadData[i].startIndex = baseTotal;
+//    cout << "[User Thread " << data->threadId << "]: " << endl << "startIndex: " << data->startIndex << endl << "numTasks: " << data->numTasks << endl;
 
-      if (extra > 0) {
-         threadData[i].numTasks = base + 1;
-         extra--;
-      } else {
-         threadData[i].numTasks = base;
-      }
+//    for (int i = 0; i < data->numTasks; i++) {
+//       // data->output[data->startIndex + i] = testFunction(data->input1[i], data->input2);
+//    }
 
-      baseTotal += threadData[i].numTasks;
+//    pthread_exit(NULL);
+// }
 
-      threadData[i].input1     = &input1;
-      cout << "HEY " << threadData[i]->input1 << endl;
-      threadData[i].input2     = &input2;
-      threadData[i].function   = function;
-      threadData[i].output     = &output;
+// template <typename T1, typename T2, typename T3>
+// void mapArray(vector<T1>& input1, vector<T2>& input2, T3 (function)(T1, vector<T2>&), vector<T3>& output)
+// {
+//    assert(input1.size() == output.size() && "Input1 and output array sizes must be equal!");
 
-      cout << "[User Program_]: Creating thread " << i << endl;
+//    int base  = input1.size() / NUM_THREADS;
+//    int extra = input1.size() % NUM_THREADS;
+//    int baseTotal = 0;
 
-      // Create our thread, and pass a pointer to it's threadDataStruct
-      int err = pthread_create(&threads[i], NULL, mapArrayThread<T1, T2, T3>, (void *)&threadData[i]);
+//    pthread_t threads[NUM_THREADS];
+//    // struct threadDataStruct<T1, T2, T3> threadData[NUM_THREADS];
+//    struct threadDataStruct threadData[NUM_THREADS];
 
-      // Checking for errors creating the thread
-      if (err) {
-         cout << "Error:unable to create thread, " << err << endl;
-         exit(-1);
-      }
-   }
-   pthread_exit(NULL);
-}
+//    for (int i = 0; i < NUM_THREADS; i++ ) {
+//       // Writing data to pass to thread
+//       threadData[i].threadId   = i;
+//       threadData[i].startIndex = baseTotal;
+
+//       if (extra > 0) {
+//          threadData[i].numTasks = base + 1;
+//          extra--;
+//       } else {
+//          threadData[i].numTasks = base;
+//       }
+
+//       baseTotal += threadData[i].numTasks;
+
+//       // threadData[i].input1     = &input1;
+
+//       // int test = input1[0];
+      
+//       // threadData[i].input2     = &input2;
+//       // threadData[i].function   = function;
+//       // threadData[i].output     = &output;
+
+//       cout << "[User Program_]: Creating thread " << i << endl;
+
+//       // Create our thread, and pass a pointer to it's threadDataStruct
+//       int err = pthread_create(&threads[i], NULL, mapArrayThread<T1, T2, T3>, (void *)&threadData[i]);
+
+//       // Checking for errors creating the thread
+//       if (err) {
+//          cout << "Error:unable to create thread, " << err << endl;
+//          exit(-1);
+//       }
+//    }
+//    pthread_exit(NULL);
+// }
 
 int main()
 {
@@ -108,7 +178,7 @@ int main()
    vector<double> output(input1.size());
 
    // Run mapArray
-   mapArray(input1, input2, testFunction, output);
+   // mapArray(input1, input2, testFunction, output);
 
    // Print input/output vectors
    for (vector<int>::const_iterator i = input1.begin(); i != input1.end(); ++i)
@@ -123,6 +193,13 @@ int main()
 
    for (vector<double>::const_iterator i = output.begin(); i != output.end(); ++i)
       cout << *i << ' ';
+
+    struct testFunction testStruct;
+    vector<int> testVect;
+
+    int o = testStruct.execute(1, testVect);
+
+    cout << endl << endl << "Testing: " << o;
 
    return 0;
 }
