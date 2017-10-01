063ae4265b71e0b19e6052a804665be1fa9111e2
Jink0 <Jink19v@gmail.com>
2017-10-01 15:45:50 +0100

    Created script for experiment 1 part 1

--- jacobi/exp1_part1.sh
+++ jacobi/exp1_part1.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # Repeats, Grid size, Num Stages, (Num workers, Num iterations, Set-pin bool, Num cores)
-bin/jacobi 1 256 1   1 10000 1 4
 
-bin/jacobi 1 256 2   1 5000  0     4 5000 0
-bin/jacobi 1 256 2   1 5000  1 4   4 5000 1 4
+NUM_RUNS=32
+
+
+STEP=$(bc -l <<< "100 / $NUM_RUNS")
+TOTAL=$STEP
+
+printf "0.000%%"
+
+for ((i=1; i<=$NUM_RUNS; i++))
+do 
+	bin/jacobi 0 256 1   $i 5000 1 32 >> /dev/null
+	printf "\r%.3f%%" "$TOTAL"
+	TOTAL=$(bc -l <<< "$TOTAL + $STEP")
+done
