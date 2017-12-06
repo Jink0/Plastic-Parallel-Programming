7b321ca62bdaf96cd568f7f8a6fcc0513086b821
Mark Jenkins <Jink19v@gmail.com>
2017-12-06 17:08:09 +0000

    Updated script

--- jacobi/scripts/optimal_threads_scripts/optimal_threads_cpu_small.sh
+++ jacobi/scripts/optimal_threads_scripts/optimal_threads_cpu_small.sh
@@ -71,6 +71,7 @@ if [ "$MACHINE" = "spa" ] ; then
     NUM_WORKERS_MAX=24
     NUM_CORES_MAX=24
     NUM_RUNS=144
+    STRING="0..11 "
     UPDATE_MOTD=false
     FILENAME="configs/spa/optimal_threads_cpu_small.ini"
     LOG_FILENAME="logs/spa/optimal_threads_cpu_small.log"
@@ -80,6 +81,7 @@ if [ "$MACHINE" = "XXXII" ] ; then
     NUM_WORKERS_MAX=48
     NUM_CORES_MAX=48
     NUM_RUNS=576
+    STRING="0..31 "
     UPDATE_MOTD=true
     FILENAME="configs/XXXII/optimal_threads_cpu_small.ini"
     LOG_FILENAME="logs/XXXII/optimal_threads_cpu_small.log"
@@ -100,14 +102,14 @@ fi
 
 echo "num_runs: \"11\"" > $FILENAME
 echo "num_stages: \"1\"" >> $FILENAME
-echo "num_iterations_0: \"10\"" >> $FILENAME
+echo "num_iterations_0: \"100\"" >> $FILENAME
 echo "set_pin_bool_0: \"2\"" >> $FILENAME
 echo "kernels_0: \"cpu\"" >> $FILENAME
 echo "kernel_durations_0: \"\"" >> $FILENAME
-echo "kernel_repeats_0: \"10000\"" >> $FILENAME 
-echo "grid_size: \"128\"" >> $FILENAME
-echo "pinnings_0: \"0..1 0..1\"" >> $FILENAME
+echo "kernel_repeats_0: \"75\"" >> $FILENAME 
+echo "grid_size: \"32\"" >> $FILENAME
 echo "num_workers_0: \"2\"" >> $FILENAME
+echo "pinnings_0: \"$STRING\"" >> $FILENAME
 
 # Overwrite log
 echo -e "Machine: $MACHINE\n\n\n\n" > $LOG_FILENAME
@@ -149,8 +151,8 @@ do
 
         head -n -2 $FILENAME > temp.ini
 
-        echo "pinnings_0: \"$FULL_STRING\"" >> temp.ini
         echo "num_workers_0: \"$j\"" >> temp.ini
+        echo "pinnings_0: \"$FULL_STRING\"" >> temp.ini
 
         mv temp.ini $FILENAME
 
