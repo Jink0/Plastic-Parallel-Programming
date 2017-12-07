ceb87ca33544d5003124602dc22d489a23c13788
Mark Jenkins <Jink19v@gmail.com>
2017-12-07 10:02:48 +0000

    Data analysis and script update

--- jacobi/scripts/optimal_threads_scripts/optimal_threads_cpu_large.sh
+++ jacobi/scripts/optimal_threads_scripts/optimal_threads_cpu_large.sh
@@ -71,6 +71,7 @@ if [ "$MACHINE" = "spa" ] ; then
     NUM_WORKERS_MAX=24
     NUM_CORES_MAX=24
     NUM_RUNS=144
+    STRING="0..11 "
     UPDATE_MOTD=false
     FILENAME="configs/spa/optimal_threads_cpu_small.ini"
     LOG_FILENAME="logs/spa/optimal_threads_cpu_small.log"
@@ -80,6 +81,7 @@ if [ "$MACHINE" = "XXXII" ] ; then
     NUM_WORKERS_MAX=48
     NUM_CORES_MAX=48
     NUM_RUNS=576
+    STRING="0..31 "
     UPDATE_MOTD=true
     FILENAME="configs/XXXII/optimal_threads_cpu_small.ini"
     LOG_FILENAME="logs/XXXII/optimal_threads_cpu_small.log"
@@ -98,16 +100,16 @@ if [ -e $FILENAME ]; then
   rm $FILENAME
 fi
 
-echo "num_runs: \"11\"" > $FILENAME
+echo "num_runs: \"101\"" > $FILENAME
 echo "num_stages: \"1\"" >> $FILENAME
-echo "num_iterations_0: \"10\"" >> $FILENAME
+echo "num_iterations_0: \"1\"" >> $FILENAME
 echo "set_pin_bool_0: \"2\"" >> $FILENAME
 echo "kernels_0: \"cpu\"" >> $FILENAME
 echo "kernel_durations_0: \"\"" >> $FILENAME
-echo "kernel_repeats_0: \"10000\"" >> $FILENAME 
-echo "grid_size: \"128\"" >> $FILENAME
-echo "pinnings_0: \"0..1 0..1\"" >> $FILENAME
+echo "kernel_repeats_0: \"1000\"" >> $FILENAME 
+echo "grid_size: \"256\"" >> $FILENAME
 echo "num_workers_0: \"2\"" >> $FILENAME
+echo "pinnings_0: \"$STRING\"" >> $FILENAME
 
 # Overwrite log
 echo -e "Machine: $MACHINE\n\n\n\n" > $LOG_FILENAME
@@ -149,22 +151,22 @@ do
 
         head -n -2 $FILENAME > temp.ini
 
-        echo "pinnings_0: \"$FULL_STRING\"" >> temp.ini
         echo "num_workers_0: \"$j\"" >> temp.ini
+        echo "pinnings_0: \"$FULL_STRING\"" >> temp.ini
 
         mv temp.ini $FILENAME
 
-    	bin/jacobi $FILENAME >> $LOG_FILENAME
+        bin/jacobi $FILENAME >> $LOG_FILENAME
         echo -e "\n\n\n\n" >> $LOG_FILENAME
 
-    	printf "\r%.3f%%" "$TOTAL"
+        printf "\r%.3f%%" "$TOTAL"
 
         if [ "$UPDATE_MOTD" = true ] ; then
             sudo scripts/update-motd.sh "$(printf "Experiments running! %.3f%% complete -Mark Jenkins (s1309061)" "$TOTAL")" >> $LOG_FILENAME
             echo -e "\n\n\n\n" >> $LOG_FILENAME
         fi
 
-    	TOTAL=$(bc -l <<< "$TOTAL + $STEP")
+        TOTAL=$(bc -l <<< "$TOTAL + $STEP")
     done
 done
 
