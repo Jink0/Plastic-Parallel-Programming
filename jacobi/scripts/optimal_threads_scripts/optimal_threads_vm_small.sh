540ea767b7161c827c3f4e9a8b8ea17cc6ca2a2c
Mark Jenkins <Jink19v@gmail.com>
2017-12-08 09:20:04 +0000

    Updated experiment scripts

--- jacobi/scripts/optimal_threads_scripts/optimal_threads_vm_small.sh
+++ jacobi/scripts/optimal_threads_scripts/optimal_threads_vm_small.sh
@@ -20,6 +20,7 @@ function ctrl_c() {
 
 DEL_PREV_RUNS=false
 MACHINE="NONE"
+RANDOMISE=false
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -35,6 +36,10 @@ case $key in
     MACHINE="$2"
     shift # past argument
     shift # past value
+    ;;
+    -r|--randomise)
+    RANDOMISE=true
+    shift # past argument
     ;;
 esac
 done
@@ -71,18 +76,20 @@ if [ "$MACHINE" = "spa" ] ; then
     NUM_WORKERS_MAX=24
     NUM_CORES_MAX=24
     NUM_RUNS=144
+    STRING="0..11 "
     UPDATE_MOTD=false
-    FILENAME="configs/spa/optimal_threads_cpu_small.ini"
-    LOG_FILENAME="logs/spa/optimal_threads_cpu_small.log"
+    FILENAME="configs/spa/optimal_threads_vm_small.ini"
+    LOG_FILENAME="logs/spa/optimal_threads_vm_small.log"
 fi
 
 if [ "$MACHINE" = "XXXII" ] ; then
     NUM_WORKERS_MAX=48
     NUM_CORES_MAX=48
     NUM_RUNS=576
+    STRING="0..31 "
     UPDATE_MOTD=true
-    FILENAME="configs/XXXII/optimal_threads_cpu_small.ini"
-    LOG_FILENAME="logs/XXXII/optimal_threads_cpu_small.log"
+    FILENAME="configs/XXXII/optimal_threads_vm_small.ini"
+    LOG_FILENAME="logs/XXXII/optimal_threads_vm_small.log"
 fi
 
 
@@ -98,16 +105,16 @@ if [ -e $FILENAME ]; then
   rm $FILENAME
 fi
 
-echo "num_runs: \"11\"" > $FILENAME
+echo "num_runs: \"101\"" > $FILENAME
 echo "num_stages: \"1\"" >> $FILENAME
-echo "num_iterations_0: \"10\"" >> $FILENAME
+echo "num_iterations_0: \"1000\"" >> $FILENAME
 echo "set_pin_bool_0: \"2\"" >> $FILENAME
-echo "kernels_0: \"cpu\"" >> $FILENAME
+echo "kernels_0: \"vm\"" >> $FILENAME
 echo "kernel_durations_0: \"\"" >> $FILENAME
-echo "kernel_repeats_0: \"10000\"" >> $FILENAME 
-echo "grid_size: \"128\"" >> $FILENAME
-echo "pinnings_0: \"0..1 0..1\"" >> $FILENAME
+echo "kernel_repeats_0: \"75\"" >> $FILENAME 
+echo "grid_size: \"32\"" >> $FILENAME
 echo "num_workers_0: \"2\"" >> $FILENAME
+echo "pinnings_0: \"$STRING\"" >> $FILENAME
 
 # Overwrite log
 echo -e "Machine: $MACHINE\n\n\n\n" > $LOG_FILENAME
@@ -121,7 +128,12 @@ START=$(date +%s.%N)
 make clean >> $LOG_FILENAME
 echo -e "\n\n\n\n" >> $LOG_FILENAME
 
-make flags="-DPTHREAD_BARRIER -DBASIC_KERNEL_SMALL -DEXECUTE_KERNELS -DCONVERGENCE_TEST" main >> $LOG_FILENAME
+if [ "$RANDOMISE" = true ] ; then
+    make flags="-DPTHREAD_BARRIER -DBASIC_KERNEL_SMALL -DRANDOMISE_KERNEL_LOAD -DEXECUTE_KERNELS -DCONVERGENCE_TEST" main >> $LOG_FILENAME
+else
+    make flags="-DPTHREAD_BARRIER -DBASIC_KERNEL_SMALL -DEXECUTE_KERNELS -DCONVERGENCE_TEST" main >> $LOG_FILENAME
+fi
+
 echo -e "\n\n\n\n" >> $LOG_FILENAME
 
 printf "0.000%%"
@@ -149,22 +161,22 @@ do
 
         head -n -2 $FILENAME > temp.ini
 
-        echo "pinnings_0: \"$FULL_STRING\"" >> temp.ini
         echo "num_workers_0: \"$j\"" >> temp.ini
+        echo "pinnings_0: \"$FULL_STRING\"" >> temp.ini
 
         mv temp.ini $FILENAME
 
-    	bin/jacobi $FILENAME >> $LOG_FILENAME
+        bin/jacobi $FILENAME >> $LOG_FILENAME
         echo -e "\n\n\n\n" >> $LOG_FILENAME
 
-    	printf "\r%.3f%%" "$TOTAL"
+        printf "\r%.3f%%" "$TOTAL"
 
         if [ "$UPDATE_MOTD" = true ] ; then
             sudo scripts/update-motd.sh "$(printf "Experiments running! %.3f%% complete -Mark Jenkins (s1309061)" "$TOTAL")" >> $LOG_FILENAME
             echo -e "\n\n\n\n" >> $LOG_FILENAME
         fi
 
-    	TOTAL=$(bc -l <<< "$TOTAL + $STEP")
+        TOTAL=$(bc -l <<< "$TOTAL + $STEP")
     done
 done
 
@@ -174,11 +186,11 @@ END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 
 if [ "$MACHINE" = "spa" ] ; then
-    sh send-encrypted.sh -k qGE5Pn -p Archimedes -s klvlqmhb -t "spa: experiment complete" -m "spa: optimal threads cpu small completed! Time taken: $DIFF seconds"
+    sh send-encrypted.sh -k qGE5Pn -p Archimedes -s klvlqmhb -t "spa: experiment complete" -m "spa: optimal threads vm small completed! Time taken: $DIFF seconds"
 fi
 
 if [ "$MACHINE" = "XXXII" ] ; then
-    sh send-encrypted.sh -k qGE5Pn -p Archimedes -s klvlqmhb -t "XXXII: experiment complete" -m "XXXII: optimal threads cpu small completed! Time taken: $DIFF seconds"
+    sh send-encrypted.sh -k qGE5Pn -p Archimedes -s klvlqmhb -t "XXXII: experiment complete" -m "XXXII: optimal threads vm small completed! Time taken: $DIFF seconds"
 fi
 
 if [ "$UPDATE_MOTD" = true ] ; then
