e1ad5a06d78c7aabf3bba0cf7d9b494619d9c052
Mark Jenkins <Jink19v@gmail.com>
2018-01-20 03:17:25 +0000

    Added cross process sync

--- jacobi/src/jacobi.cpp
+++ jacobi/src/jacobi.cpp
@@ -119,7 +119,112 @@ static uint32_t const border_size = 2;
 
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+#define NUM_PROCS_TO_SYNC 2
+
+char mutex_sem_name[] = "jacobi_mutex_sem";
+char counter_sem_name[] = "jacobi_counter_sem";
+char turnstile1_sem_name[] = "jacobi_turnstile1_sem";
+char turnstile2_sem_name[] = "jacobi_turnstile2_sem";
+sem_t *mutex_sem, *counter_sem, *turnstile1_sem, *turnstile2_sem;
+
+void init_cross_proc_barrier() {
+    if ((mutex_sem = sem_open(mutex_sem_name, O_CREAT, 0644, 1)) == SEM_FAILED) {
+        perror("named semaphore initialization error");
+        exit(1);
+    }
+
+    if ((counter_sem = sem_open(counter_sem_name, O_CREAT, 0644, 0)) == SEM_FAILED) {
+        perror("named semaphore initialization error");
+        exit(1);
+    }
+
+    if ((turnstile1_sem = sem_open(turnstile1_sem_name, O_CREAT, 0644, 0)) == SEM_FAILED) {
+        perror("named semaphore initialization error");
+        exit(1);
+    }
+
+    if ((turnstile2_sem = sem_open(turnstile2_sem_name, O_CREAT, 0644, 1)) == SEM_FAILED) {
+        perror("named semaphore initialization error");
+        exit(1);
+    }
+}
+
+void cross_proc_barrier() {
+    sem_wait(mutex_sem);
+
+    sem_post(counter_sem);
+
+    int count;
+    sem_getvalue(counter_sem, &count);
+
+    if (count == NUM_PROCS_TO_SYNC) {
+
+        sem_wait(turnstile2_sem);
+
+        sem_post(turnstile1_sem);
+    }
+
+    sem_post(mutex_sem);
+
+    sem_wait(turnstile1_sem);
+
+    sem_post(turnstile1_sem);
+
+    sem_wait(mutex_sem);
+
+    sem_trywait(counter_sem);
+
+    sem_getvalue(counter_sem, &count);
+
+    if (count == 0) {
+
+        sem_wait(turnstile1_sem);
+
+        sem_post(turnstile2_sem);
+    }
+
+    sem_post(mutex_sem);
+
+    sem_wait(turnstile2_sem);
+
+    sem_post(turnstile2_sem);
+}
+
+void close_cross_proc_barrier() {
+    sem_close(mutex_sem);
+    sem_close(counter_sem);
+    sem_close(turnstile1_sem);
+    sem_close(turnstile2_sem);
+}
+
+void unlink_cross_proc_barrier() {
+    sem_unlink(mutex_sem_name);
+    sem_unlink(counter_sem_name);
+    sem_unlink(turnstile1_sem_name);
+    sem_unlink(turnstile2_sem_name);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main(int argc, char *argv[]) {
+
+	init_cross_proc_barrier();
 
 	// Parse config
 	std::map<std::string, std::string> config = parse_config(std::string(argv[1]));
@@ -192,8 +297,6 @@ int main(int argc, char *argv[]) {
 		pthread_barriers.push_back(b);
 	}
 
-
-
 	// Initialize run times sum for computing average
 	uint32_t run_times_sum = 0;
 
@@ -201,6 +304,8 @@ int main(int argc, char *argv[]) {
 
 		initialize_grids();
 
+		cross_proc_barrier();
+         
 		// Record start time
 		std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 
@@ -234,6 +339,10 @@ int main(int argc, char *argv[]) {
 
 	// Print average runtime
 	print("\nAverage elapsed time over ", num_runs, " runs: ", run_times_sum / num_runs, "ms\n");
+
+	// Close and unlink semaphore
+	close_cross_proc_barrier();
+    unlink_cross_proc_barrier();
 }
 
 
